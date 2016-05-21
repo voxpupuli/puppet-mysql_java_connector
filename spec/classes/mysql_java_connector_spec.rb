3a683ce997d4092f3dd608e39ddb00109e0f881d
Tim Meusel <tim@bastelfreak.de>
2016-05-21 23:47:01 +0200

    rubocop: fix Style/SpaceInsideBrackets

--- spec/classes/mysql_java_connector_spec.rb
+++ spec/classes/mysql_java_connector_spec.rb
@@ -33,7 +33,7 @@ describe 'mysql_java_connector' do
             :version     => '4.99.111',
             :installdir  => '/opt/custom',
             :downloadURL => 'http://example.co.za',
-            :links       => [ '/opt/tomcat_app/lib', '/opt/jboss_app/lib' ]
+            :links       => ['/opt/tomcat_app/lib', '/opt/jboss_app/lib']
           }}
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_class('mysql_java_connector') }
