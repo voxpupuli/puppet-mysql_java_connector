b01a2a573523f5c204f47c496d45703972d3d75d
DrRhythm <8515270+DrRhythm@users.noreply.github.com>
2022-09-07 16:39:37 -0700

    adding version comparison for connector symlink
    Removing trailing whitespace on lines 11 and 17

--- manifests/links.pp
+++ manifests/links.pp
@@ -7,8 +7,16 @@ define mysql_java_connector::links (
   $product    = $mysql_java_connector::product,
   $version    = $mysql_java_connector::version,
 ) {
+  # ref: https://github.com/mysql/mysql-connector-j/commit/d2f8baa53ca3e82e2d161b3af03e0cf13fb4530c
+  if versioncmp($version, '8.0.11') >= 0 {
+    $link_target = "${installdir}/latest/${product}-${version}.jar"
+  }
+  else {
+    $link_target = "${installdir}/latest/${product}-${version}-bin.jar"
+  }
+
   file { "${name}/mysql-connector-java.jar":
     ensure => link,
-    target => "${installdir}/latest/${product}-${version}-bin.jar",
+    target => $link_target,
   }
 }
